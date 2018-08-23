6900525beaadf915b609f751d4710f3c85ee3ba7
Jenny Crowther <jenny@batch.nz>
2018-08-23 21:33:39 +1200

    style

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Default Starter with Tailwind!',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
